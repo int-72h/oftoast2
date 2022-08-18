9c63d1a198a1fcbabc448c3c1f105fcf78e9e50e
int-72h <int-10h@protonmail.com>
2022-08-18 01:49:47 +0100

    initial commit

---
+++ Main.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_Icon_pressed():
+	pass # Replace with function body.
+
+
+func _on_Update_pressed():
+	pass # Replace with function body.
+
+
+func _on_Verify_pressed():
+	pass # Replace with function body.
