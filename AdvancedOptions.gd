3cff80deba7eb36979c52b3b78ff96826217da03
intcoms <int@kmatter.net>
2022-11-21 12:25:46 +0000

    forgot this

---
+++ AdvancedOptions.gd
@@ -0,0 +1,24 @@
+extends ScrollContainer
+onready var target_rev = $MarginContainer/VBoxContainer/VersionContainer/Revision/Text
+onready var threads = $MarginContainer/VBoxContainer/LauncherContainer/VBoxContainer/Threads/Text
+onready var maxdl = $MarginContainer/VBoxContainer/LauncherContainer/VBoxContainer/MaxDLSpeed/Text
+onready var inst_dir = $MarginContainer/VBoxContainer/LauncherContainer/VBoxContainer/InstallDir/Text
+onready var mirrors = $MarginContainer/VBoxContainer/LauncherContainer/VBoxContainer/Mirrors/Text
+
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
